5de028735a1574210ea49ac304b3a7a9d950c5e3
sarra <gusmi.sarra@gmail.com>
2021-04-20 13:39:02 +0100

    heroku config

--- config/connectDB.js
+++ config/connectDB.js
@@ -2,10 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_URI, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
+    await mongoose.connect(
+      process.env.DB_URI ||
+        "mongodb+srv://sarra:sarra123456789@cluster0.wpvhb.mongodb.net/auth_ws?retryWrites=true&w=majority",
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      }
+    );
     console.log("database connected successfully");
   } catch (error) {
     console.log("database can not connect", error);
--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const express = require("express");
 const connectDB = require("./config/connectDB");
 require("dotenv").config();
-
+const path = require("path");
 const app = express();
 
 connectDB();
-
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static("client/build"));
-}
 
 // middleware global
 app.use(express.json());
 // router
 app.use("/api/user", require("./router/user"));
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT;
 
