e126d88613ef7588bd96bd0e7a1bc7558c7a5932
sarra <gusmi.sarra@gmail.com>
2021-04-20 14:15:01 +0100

    heroku config

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/connectDB");
-require("dotenv").config();
+// require("dotenv").config();
 const path = require("path");
 const app = express();
 
